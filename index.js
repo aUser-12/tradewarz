e6619262c53de22e0a967870b913bf4a373a1282
SaiPrajwal583 <justforplayinggenshinimpactacc@gmail.com>
2025-07-10 20:36:25 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
 // index.js
+import { authorize , signout , change_password } from "./routes/auth"
 const fastify = require('fastify')({ logger: true })
+const fastifyCookie = require('@fastify/cookie');
+const fastifySession = require('@fastify/session');
+const cron = require('node-cron')
+const { processMatches } = require('elo')
+fastify.register(fastifyCookie);
+fastify.register(fastifySession, {
+  secret: 'akhilandjimitsittingunderatree',
+  cookie: { secure: false }, // set to true in production with HTTPS
+  saveUninitialized: false
+});
+cron.schedule('*/25 * * * *', () => {
+  processMatches();
+}); 
 
 fastify.get('/', async (request, reply) => {
-  return { hello: 'world' }
+  return { hello: 'hello world' }
 })
 
 fastify.listen({ port: 3000 }, (err, address) => {
