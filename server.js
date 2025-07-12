ebd3805e987394808013e89752011f9d56881a5a
SaiPrajwal583 <justforplayinggenshinimpactacc@gmail.com>
2025-07-12 21:54:37 +0530

    made the signup js

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 // index.js
 import { authorize , signout , change_password } from "./routes/auth"
 const fastify = require('fastify')({ logger: true })
+const path = require('path');
+const formBody = require('@fastify/formbody');
 const fastifyCookie = require('@fastify/cookie');
 const fastifySession = require('@fastify/session');
 const cron = require('node-cron')
 const { processMatches } = require('elo')
+fastify.register(formBody);
 fastify.register(fastifyCookie);
 fastify.register(fastifySession, {
   secret: 'akhilandjimitsittingunderatree',
@@ -14,10 +17,19 @@ fastify.register(fastifySession, {
 cron.schedule('*/25 * * * *', () => {
   processMatches();
 }); 
-
-fastify.get('/', async (request, reply) => {
-  reply.sendFile('index.html');
+fastify.register(require('@fastify/static'), {
+  root: path.join(__dirname, 'static'),
+  prefix: '/', // URL prefix
+});
+fastify.get('/signup', async (request, reply) => {
+  reply.sendFile('Sign in.html');
 })
+fastify.post('/signup', async (request, reply) => {
+  const { username, password } = request.body;
+  console.log('Form Data:', username, password);
+
+  return reply.send({ success: true, username });
+});
 fastify.listen({ port: 3000 }, (err, address) => {
   if (err) throw err
   console.log(` Server running at ${address}`)
