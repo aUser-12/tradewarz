f0ebbc174d2bd2787eabd838bb20da818743a395
SaiPrajwal583 <justforplayinggenshinimpactacc@gmail.com>
2025-07-13 20:04:50 +0530

    fixed all the mistakes akhil made

--- server.js
+++ server.js
@@ -1,49 +1,64 @@
-// index.js
-const fastify = require('fastify')({ logger: true })
+const fastify = require('fastify')({ logger: true });
 const path = require('path');
 const formBody = require('@fastify/formbody');
 const fastifyCookie = require('@fastify/cookie');
 const fastifySession = require('@fastify/session');
-const cron = require('node-cron')
+const cron = require('node-cron');
 const { processMatches } = require('./elo');
-const {authorize, change_password, signout } = require('./routes/auth')
+const { authorize, change_password, signout } = require('./routes/auth');
+
 fastify.register(formBody);
 fastify.register(fastifyCookie);
 fastify.register(fastifySession, {
   secret: 'onetwothreefourfivesixseveneightnineteneleventwelve',
-  cookie: { secure: false }, // set to true in production with HTTPS
+  cookie: { secure: false },
   saveUninitialized: false
 });
 
 cron.schedule('*/25 * * * *', () => {
   processMatches();
-}); 
+});
+
 fastify.register(require('@fastify/static'), {
   root: path.join(__dirname, 'static'),
-  prefix: '/', // URL prefix
+  prefix: '/',
 });
+
 fastify.get('/', async (request, reply) => {
   return reply.redirect('/signup');
 });
+
 fastify.get('/signup', async (request, reply) => {
-  return reply.sendFile('auth.html');
-})
+  return reply.sendFile('signup.html');
+});
+
 fastify.post('/signup', async (request, reply) => {
   const { username, password } = request.body;
-  console.log('Form Data:', username, password);
-  authorize("register",username,password)
-  return reply.send({ success: true, username });
+  const result = await authorize("register", username, password);
+  return reply.send(result);
 });
+
 fastify.get('/auth', async (request, reply) => {
-  return reply.sendFile('signup.html');
-})
+  return reply.sendFile('login.html');
+});
+
 fastify.post('/auth', async (request, reply) => {
   const { username, password } = request.body;
-  console.log('Form Data:', username, password);
-  authorize("login",username,password)
-  return reply.send({ success: true, username });
+  const result = await authorize("login", username, password);
+  return reply.send(result);
 });
+
+fastify.get('/changepass', async (request, reply) => {
+  return reply.sendFile('change-password.html');
+});
+
+fastify.post('/changepass', async (request, reply) => {
+  const { username, currentPass, newPass } = request.body;
+  const result = await change_password(username, currentPass, newPass);
+  return reply.send(result);
+});
+
 fastify.listen({ port: 3000 }, (err, address) => {
-  if (err) throw err
-  console.log(` Server running at ${address}`)
-})
+  if (err) throw err;
+  console.log(` Server running at ${address}`);
+});
